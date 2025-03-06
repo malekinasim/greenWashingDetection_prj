35e4672565f0e1b5a0d21ca91ca4f42445b67eff
nasim maleki <maleki.nasim@gmail.com>
2025-03-06 20:57:00 +0100

    change rgb detector add new extracing method and add greewashing score detector

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from  src.pipeline import PDFPipeline
 from src.config import PDF_FOLDER_PATH,IMAGE_FOLDER_PATH,EXCEL_OUTPUT_FOLDER_PATH
 def main():
-   PDFPipeline(PDF_FOLDER_PATH, IMAGE_FOLDER_PATH,EXCEL_OUTPUT_FOLDER_PATH, method='PYMUPDF').process_pdfs(True)
-   PDFPipeline(PDF_FOLDER_PATH, IMAGE_FOLDER_PATH,EXCEL_OUTPUT_FOLDER_PATH, method='OPENCV').process_pdfs(True)
+   #    PDFPipeline(PDF_FOLDER_PATH, IMAGE_FOLDER_PATH,EXCEL_OUTPUT_FOLDER_PATH, method='PYMUPDF').process_pdfs(True)
+   #    PDFPipeline(PDF_FOLDER_PATH, IMAGE_FOLDER_PATH,EXCEL_OUTPUT_FOLDER_PATH, method='OPENCV').process_pdfs(True)
+   # PDFPipeline(PDF_FOLDER_PATH, IMAGE_FOLDER_PATH,EXCEL_OUTPUT_FOLDER_PATH, method='SPAREPDF').process_pdfs(True)
+   PDFPipeline(PDF_FOLDER_PATH, IMAGE_FOLDER_PATH,EXCEL_OUTPUT_FOLDER_PATH, method='COMBINE').process_pdfs(True)
 #
 #  Run the pipelin
-
 main()
